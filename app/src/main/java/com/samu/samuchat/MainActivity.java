5cf6b32054c1a20d68f328db2b71b99d1651e02c
Atreuss0 <arzumuhammet5@gmail.com>
2022-06-14 21:00:39 +0300

    kayıt kısmı

--- app/src/main/java/com/samu/samuchat/MainActivity.java
+++ app/src/main/java/com/samu/samuchat/MainActivity.java
@@ -19,6 +19,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.samu.samuchat.databinding.ActivityMainBinding;
@@ -31,6 +32,7 @@ private ActivityMainBinding binding;
 
     private EditText editemail,editsifre,editisim;
     private String txtemail,txtsifre,txtisim;
+    private FirebaseUser mUser;
     private FirebaseAuth mAuth;
     private DatabaseReference mReferans;
     private HashMap <String,Object> mData;
@@ -59,7 +61,8 @@ private ActivityMainBinding binding;
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-    public void kayıt_ekrani(View v)
+
+    public void Kayıtol(View v)
     {
         txtemail=editemail.getText().toString();
         txtsifre=editsifre.getText().toString();
@@ -72,12 +75,23 @@ private ActivityMainBinding binding;
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful())
                     {
+                        mUser=mAuth.getCurrentUser();
+
                         mData=new HashMap<>();
                         mData.put("kullanıcı adı",txtisim);
                         mData.put("kullanıcı email",txtemail);
                         mData.put("kullanıcı sifresi",txtsifre);
+                        mData.put("kullanıcı Uid",mUser.getUid());
 
-                        mReferans.child("kullanıcılar").setValue(mData);
+                        mReferans.child("kullanıcılar").child(mUser.getUid())
+                                .setValue(mData)
+                                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful())
+                                            Toast.makeText( MainActivity.this,"kayıt ıslemı basarılı", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
                     }
                     else
                     {
