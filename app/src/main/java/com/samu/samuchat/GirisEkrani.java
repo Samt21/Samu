b574a4bf95d209cd827d8bfbcaf1d325b295dac1
Atreuss0 <arzumuhammet5@gmail.com>
2022-06-14 19:52:44 +0300

    kayıt kısmı

--- app/src/main/java/com/samu/samuchat/GirisEkrani.java
+++ app/src/main/java/com/samu/samuchat/GirisEkrani.java
@@ -1,22 +1,41 @@
 package com.samu.samuchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class GirisEkrani extends AppCompatActivity {
 
     Button btn_giris;
     Button btn_kayit;
+    private EditText giriş_yap_email,Giriş_yap_parola;
+    private String txtemail,txtsifre;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giris_ekrani);
 
+        giriş_yap_email=(EditText)findViewById(R.id.giriş_yap_email);
+        Giriş_yap_parola=(EditText)findViewById(R.id.Giriş_yap_parola);
+
+        mAuth=FirebaseAuth.getInstance();
 
         btn_giris=findViewById(R.id.btn_girisyap);
         btn_giris.setOnClickListener(new View.OnClickListener() {
@@ -26,7 +45,6 @@ public class GirisEkrani extends AppCompatActivity {
                 startActivities(new Intent[]{iris});
             }
         });
-
         btn_kayit=findViewById(R.id.btn_kayitol);
         btn_kayit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,12 +52,34 @@ public class GirisEkrani extends AppCompatActivity {
                 Intent kayit=new Intent(GirisEkrani.this,KayitEkrani.class);
                 startActivities(new Intent[]{kayit});
             }
-
-
         });
-
     }
+    public void GirisEkranı(View view)
+    {
+        txtemail=giriş_yap_email.getText().toString();
+        txtsifre=Giriş_yap_parola.getText().toString();
 
+        if(!TextUtils.isEmpty(txtemail) &&!TextUtils.isEmpty(txtsifre))
+        {
+            mAuth.signInWithEmailAndPassword(txtemail,txtsifre).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    mUser=mAuth.getCurrentUser();
+
+                    System.out.println("Kullanıcı Adı :"+mUser.getDisplayName());
+                    System.out.println("Kullanıcı Email :"+mUser.getEmail());
+
+                }
+            }).addOnFailureListener(this, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(GirisEkrani.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else
+            Toast.makeText(this,"email ve şifre boş olamaz",Toast.LENGTH_SHORT).show();
+    }
     
 
 
--- app/src/main/java/com/samu/samuchat/MainActivity.java
+++ app/src/main/java/com/samu/samuchat/MainActivity.java
@@ -1,21 +1,50 @@
 package com.samu.samuchat;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.samu.samuchat.databinding.ActivityMainBinding;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
 private ActivityMainBinding binding;
 
+    private EditText editemail,editsifre,editisim;
+    private String txtemail,txtsifre,txtisim;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mReferans;
+    private HashMap <String,Object> mData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        editemail=(EditText)findViewById(R.id.email);
+        editsifre=(EditText)findViewById(R.id.sifre);
+        editisim=(EditText)findViewById(R.id.isim);
+
+
+        mAuth=FirebaseAuth.getInstance();
+        mReferans= FirebaseDatabase.getInstance().getReference();
 
      binding = ActivityMainBinding.inflate(getLayoutInflater());
      setContentView(binding.getRoot());
@@ -29,6 +58,38 @@ private ActivityMainBinding binding;
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+    public void kayıt_ekrani(View v)
+    {
+        txtemail=editemail.getText().toString();
+        txtsifre=editsifre.getText().toString();
+        txtisim=editisim.getText().toString();
+
+        if(!TextUtils.isEmpty(txtemail) &&!TextUtils.isEmpty(txtsifre))
+        {
+            mAuth.createUserWithEmailAndPassword(txtemail,txtsifre).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        mData=new HashMap<>();
+                        mData.put("kullanıcı adı",txtisim);
+                        mData.put("kullanıcı email",txtemail);
+                        mData.put("kullanıcı sifresi",txtsifre);
+
+                        mReferans.child("kullanıcılar").setValue(mData);
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, (CharSequence) task.getException(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+        else{
+            Toast.makeText(this,"email ve şifre boş olamaz",Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
